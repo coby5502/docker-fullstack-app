1026d39b4cf096bda2673d4ade63f0195adcd0d5
Doyoung <coby5502@gmail.com>
2021-07-21 17:57:58 +0900

    fixed2

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 const pool = mysql.createPool({
     connectionLimit: 10,
     host: process.env.MYSQL_HOST,
-    user: rocess.env.MYSQL_USER,
+    user: process.env.MYSQL_USER,
     password: process.env.MYSQL_ROOT_PASSWORD,
     database: process.env.MYSQL_DATABASE,
     port: process.env.MYSQL_PORT
